78d52cfe680fecb81cdbdb213b613b2ad77521ab
Earl Jay Caoile <unfie555@gmail.com>
2020-03-29 12:42:11 -0700

    defined mongoose variable

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 require('dotenv').config();
+const mongoose = require('mongoose');
 const server = require('./lib/server.js');
+
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 server.start(process.env.PORT || 3000);
