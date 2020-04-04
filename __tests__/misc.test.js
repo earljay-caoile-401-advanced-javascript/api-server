a79906dd9767c4f5e0443e80bb4149fb8a1bc745
Earl Jay Caoile <unfie555@gmail.com>
2020-04-03 18:51:39 -0700

    addded test for invalid model

--- __tests__/misc.test.js
+++ __tests__/misc.test.js
@@ -2,16 +2,23 @@
 
 const supergoose = require('@code-fellows/supergoose');
 const server = require('../lib/server.js');
-supergoose(server.apiServer);
+const agent = supergoose(server.apiServer);
 console.log = jest.fn();
 
 describe('server', () => {
   it('will console log on start', () => {
     jest.spyOn(global.console, 'log');
-    server.apiServer.listen = jest.fn(port => {
+    server.apiServer.listen = jest.fn((port) => {
       console.log('running on', port);
     });
     server.start(3000);
     expect(console.log).toHaveBeenCalled();
+  });
+});
+
+describe('model finder', () => {
+  it('will return an error for an invalid model', async () => {
+    const getRes = await agent.get('/api/v1/dumplings');
+    expect(getRes.statusCode).toBe(500);
   });
 });
